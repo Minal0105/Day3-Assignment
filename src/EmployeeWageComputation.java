1dcef46eea22a3c0c6d7bbe2ab43b09271bc435d
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 19:22:47 +0530

    Added : Code to Add Part Time Employee Wage Using Switch Case

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,19 +1,23 @@
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        int Full_time=1;
-        int Part_time=2;
-        int wage_per_hour=20;
-        int emp_hours;
-        int emp_wage;
-        int empcheck=(int)((Math.random()*10)%3);
-        if(empcheck==Part_time){
-            emp_hours=8;
+    public static final int Full_time=1;
+    public static final int Part_time=2;
+    public static final int Emp_rate_per_hour=20;
+    public static void main(String[] args)
+    {
+        int emp_hours=0;
+        int emp_wage=0;
+        int empcheck=(int)((Math.random()*10)%4);
+        switch(empcheck){
+            case Full_time :
+                emp_hours=9;
+                break;
+            case Part_time :
+                emp_hours=8;
+                break;
+            default :
+                emp_hours=0;
         }
-        else if(empcheck==Full_time) {
-            emp_hours=10;
-        }
-        else emp_hours=0;
-        emp_wage=emp_hours*wage_per_hour;
+        emp_wage=emp_hours*Emp_rate_per_hour;
         System.out.println("Emp_wage : "+emp_wage);
     }
 }
