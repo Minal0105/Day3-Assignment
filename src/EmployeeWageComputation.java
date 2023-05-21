41a4d413d8d0a3882c42b20b7ba3afa736f3eafd
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 19:09:45 +0530

    Added : Code to Add Part Time Employee Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,15 +1,20 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        int wage_per_hour = 20;
-        int empcheck = (int) ((Math.random() * 10) % 2);
-        int emp_hours = 0;
-        int emp_wage = 0;
-        if (empcheck == 1) {
-            emp_hours = 8;
-        } else {
-            emp_hours = 0;
+        int Full_time=1;
+        int Part_time=2;
+        int wage_per_hour=20;
+        int emp_hours;
+        int emp_wage;
+        int empcheck=(int)((Math.random()*10)%3);
+        if(empcheck==Part_time){
+            emp_hours=8;
+
         }
-        emp_wage = emp_hours * wage_per_hour;
-        System.out.println("EmpWage :" + emp_wage);
+        else if(empcheck==Full_time) {
+            emp_hours=10;
+        }
+        else emp_hours=0;
+        emp_wage=emp_hours*wage_per_hour;
+        System.out.println("Emp_wage : "+emp_wage);
     }
 }
