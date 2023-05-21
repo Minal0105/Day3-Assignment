4ab99a01dc17031aad0a0cda668aa94bcffc1e99
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 19:42:25 +0530

    Added : Code to calculate totle working hours of an employee for a month

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,16 +1,18 @@
 public class EmployeeWageComputation {
-    public static final int Full_time = 1;
-    public static final int Part_time = 2;
+    public static final int Part_time = 1;
+    public static final int Full_time = 2;
     public static final int Emp_rate_per_hour = 20;
     public static final int No_of_working_days = 20;
+    public static final int Max_hours_in_month = 100;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         int emp_hours = 0;
-        int emp_wage = 0;
+        int Totle_emp_hours = 0;
+        int Totle_working_days = 0;
         int Totle_emp_wage = 0;
-        for (int day = 0; day < No_of_working_days; day++) {
+        while (Totle_emp_hours<=Max_hours_in_month && Totle_working_days < No_of_working_days) {
+            Totle_working_days++;
             int empcheck = (int) ((Math.random() * 10) % 3);
             switch (empcheck) {
                 case Part_time:
@@ -18,13 +20,14 @@ public class EmployeeWageComputation {
                     break;
                 case Full_time:
                     emp_hours = 8;
+                    break;
                 default:
                     emp_hours = 0;
             }
-            emp_wage = emp_hours * Emp_rate_per_hour;
-            Totle_emp_wage += emp_wage;
-            System.out.println("Emp_wage : " + emp_wage);
+            Totle_emp_hours += emp_hours;
+            System.out.println("Days :"+Totle_working_days + " EmpHours :" +emp_hours);
         }
+        Totle_emp_wage=Totle_emp_hours * Emp_rate_per_hour;
         System.out.println("Totle Emp Wage" + Totle_emp_wage);
     }
 }
