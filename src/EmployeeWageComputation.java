a1a689b348276aa8ef1eff6c9cafd8720e6adacd
minal1593 <minalsalunkhe15@gmail.com>
2023-05-21 19:01:09 +0530

    Added : Code to check Daily Employee Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int wage_per_hour = 20;
         int empcheck = (int) ((Math.random() * 10) % 2);
         int emp_hours = 0;
