ee1510fa84cea7d90e5d8d6d7e36da2f77e3f2f3
minal1593 <minalsalunkhe15@gmail.com>
2023-06-11 21:28:06 +0530

    Added : Refactored Code

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,34 +1,40 @@
 public class EmployeeWageComputation {
-    public static final int Part_time = 1;
-    public static final int Full_time = 2;
-    public static final int Emp_rate_per_hour = 20;
-    public static final int No_of_working_days = 20;
-    public static final int Max_hours_in_month = 100;
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NO_OF_WORKING_DAYS = 20;
+    public static int calculateEmployeeWage(int empCheck) {
+        int empHours = 0;
+
+        switch (empCheck) {
+            case PART_TIME:
+                empHours = 4;
+                break;
+            case FULL_TIME:
+                empHours = 8;
+                break;
+            default:
+                empHours = 0;
+                break;
+        }
+
+        return empHours * EMP_RATE_PER_HOUR;
+    }
 
     public static void main(String[] args) {
+        int empHours = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
 
-        int emp_hours = 0;
-        int Totle_emp_hours = 0;
-        int Totle_working_days = 0;
-        int Totle_emp_wage = 0;
-        while (Totle_emp_hours<=Max_hours_in_month && Totle_working_days < No_of_working_days) {
-            Totle_working_days++;
-            int empcheck = (int) ((Math.random() * 10) % 3);
-            switch (empcheck)
-            {
-                case Part_time:
-                    emp_hours = 4;
-                    break;
-                case Full_time:
-                    emp_hours = 8;
-                    break;
-                default:
-                    emp_hours = 0;
-            }
-            Totle_emp_hours += emp_hours;
-            System.out.println("Days :"+Totle_working_days + " EmpHours :" +emp_hours);
+        for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) ((Math.random() * 10) % 3);
+            empHours = calculateEmployeeWage(empCheck);
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+
+            System.out.println("Emp_wage : " + empWage);
         }
-        Totle_emp_wage=Totle_emp_hours * Emp_rate_per_hour;
-        System.out.println("Totle Emp Wage" + Totle_emp_wage);
+
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
